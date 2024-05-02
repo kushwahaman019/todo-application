ae44107c3ade1d8086213e04bf9d3135030385ee
kushwahaman019 <130533432+kushwahaman019@users.noreply.github.com>
2024-05-02 20:46:57 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+const express=require("express");
+const app=express();
+app.use(express.json());
+app.post("/todo",function(){
+
+});
+app.get("/todo",function(){
+
+});
+app.put("/completed",function(
+){
+
+});
